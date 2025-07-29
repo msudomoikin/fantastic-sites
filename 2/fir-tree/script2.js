e6aae9b613cbb7f78405c8c5cf25c2ff77b5f8b1
Matvey Sudomoykin <me@matveymatvey.com>
2025-07-30 02:29:35 +0300

    add toys to tree

--- 2/fir-tree/script2.js
+++ 2/fir-tree/script2.js
@@ -12,17 +12,31 @@ function createTreeSegment(height, symbol = '^', maxHeight) {
 
 
     for (let index = 0; index < height; index++) {
-        let segment = ''
+        let segment = '';
+        let canHangToy = false;
 
         segment += '/'.repeat(maxHeight - index - 1)
 
         let symbolCount = 1 + index * 2;
+
+        symbolCount > 2 ? canHangToy = true : canHangToy = false;
+
         for (let j = 0; j < symbolCount; j++) {
-            segment += symbol
+            const randomNum = getRandomInt(symbolCount)
+            if (randomNum === index && canHangToy) {
+                segment += '0';
+                canHangToy = false;
+            } else {
+                segment += symbol;
+            }
         }
 
-        segment += '\\'.repeat(maxHeight - index - 1)
+        segment += '\\'.repeat(maxHeight - index - 1);
 
-        console.log(segment)
+        console.log(segment);
     }
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
 }
