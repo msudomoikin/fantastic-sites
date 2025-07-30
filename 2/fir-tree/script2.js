ce75f2413eef7675db0c1fe7e8cfb6754f42e9e6
Matvey Sudomoykin <me@matveymatvey.com>
2025-07-30 13:20:17 +0300

    add toy parameter

--- 2/fir-tree/script2.js
+++ 2/fir-tree/script2.js
@@ -1,22 +1,20 @@
-
-
 createTree();
 
 function createTree() {
-    const maxHeight = 9
-    createTreeSegment(1, '*', maxHeight)
-    createTreeSegment(3, '^', maxHeight)
-    createTreeSegment(5, '^', maxHeight)
-    createTreeSegment(9, '^', maxHeight)
+    const treeHeight = 9
+    createTreeSegment(1, '*', '', treeHeight)
+    createTreeSegment(3, '^', '❂', treeHeight)
+    createTreeSegment(5, '^', '⚉', treeHeight)
+    createTreeSegment(9, '^', '◍', treeHeight)
 }
 
 
-function createTreeSegment(height, symbol = '^', maxHeight) {
+function createTreeSegment(height, treeSymbol = '^', toy = 'o', treeHeight) {
     for (let index = 0; index < height; index++) {
         let segment = '';
         let canHangToy = false;
 
-        segment += ' '.repeat(maxHeight - index - 1)
+        segment += ' '.repeat(treeHeight - index - 1)
 
         let symbolCount = 1 + index * 2;
 
@@ -25,15 +23,12 @@ function createTreeSegment(height, symbol = '^', maxHeight) {
         for (let j = 0; j < symbolCount; j++) {
             const randomNum = getRandomInt(symbolCount)
             if (randomNum === index && canHangToy) {
-                segment += 'o';
-                canHangToy = false; // only one toy
+                segment += toy;
+                canHangToy = false; // lets add only one toy
             } else {
-                segment += symbol;
+                segment += treeSymbol;
             }
         }
-
-        // segment += '\\'.repeat(maxHeight - index - 1);
-
         console.log(segment);
     }
 }
